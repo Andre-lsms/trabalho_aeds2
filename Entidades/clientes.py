bd87a256d5bae87f756f365cc757b8f39cab9e46
André Santos <andreluismagalhaes14@gmail.com>
2025-01-09 15:48:14 -0300

    Modificação classe CLientes
    Modifiquei para que se adaptasse a classe pai

--- Entidades/clientes.py
+++ Entidades/clientes.py
@@ -1,39 +1,35 @@
-import struct
-
-from faker import Faker
-
-fake = Faker('pt_BR')
+from  Entidades.entidade_base import EntidadeBase
+from Entidades import funcoes
 
 
-class Cliente:
-    def criarar_cliente(self, cod):
+
+
+class Cliente(EntidadeBase):
+    @classmethod
+    def get_formato(cls):
+        # Define o formato do registro para Cliente
+        return '=i30si14s70s20s30s'
+    def criar_registro(self, cod, arquivo):
         codigo = cod
-        nome = (fake.name())[:30]
-        idade = fake.random_int(min=18, max=70)
-        cpf = (fake.numerify(text='###.###.###-##'))[:14]
-        endereco = (self.gerar_endereco())[:70]
-        telefone = (fake.phone_number())[:15]
-        email = (fake.email())[:30]
+        nome = (self.fake.name())[:30]
+        idade = self.fake.random_int(min=18, max=70)
+        cpf = (self.fake.numerify(text='###.###.###-##'))[:14]
+        endereco = (funcoes.gerar_endereco())[:70]
+        telefone = (self.fake.cellphone_number())[:20]
+        email = (self.fake.email())[:30]
 
-        return codigo, nome, idade, cpf, endereco, telefone, email
+        return codigo, nome.encode('utf-8'), idade, cpf.encode('utf-8'), endereco.encode('utf-8'), telefone.encode(
+            'utf-8'), email.encode('utf-8')
+
+    def imprimir_registro(self, registro):
+        registro_lido = self.ler_registro(registro)
+        print(f"Codigo: [{registro_lido[0]}]")
+        print(f"Nome: {registro_lido[1].decode('utf-8').rstrip(chr(0))}")
+        print(f"Idade: {registro_lido[2]}")
+        print(f"CPF: {registro_lido[3].decode('utf-8').rstrip(chr(0))}")
+        print(f"Endereco: {registro_lido[4].decode('utf-8').rstrip(chr(0))}")
+        print(f"Telefone: {registro_lido[5].decode('utf-8').rstrip(chr(0))}")
+        print(f"Email: {registro_lido[6].decode('utf-8').rstrip(chr(0))}")
+        print(f'{80 * "-"}')
 
 
-    def salvar_cliente(self, arquivo, cod):
-        cliente = self.criarar_cliente(cod)
-        arquivo.write(struct.pack('i', cliente[0]))
-        arquivo.write(struct.pack(f'30s', cliente[1].encode('utf-8')))
-        arquivo.write(struct.pack('i', cliente[2]))
-        arquivo.write(struct.pack('14s', cliente[3].encode('utf-8')))
-        arquivo.write(struct.pack('70s', cliente[4].encode('utf-8')))
-        arquivo.write(struct.pack('15s', cliente[5].encode('utf-8')))
-        arquivo.write(struct.pack('30s', cliente[6].encode('utf-8')))
-
-
-
-    @staticmethod
-    def gerar_endereco():
-        rua = fake.street_name()
-        numero = fake.building_number()
-        bairro = fake.bairro()
-        cidade = fake.city()
-        return rua + ', ' + numero + ' - ' + bairro + '. ' + cidade
