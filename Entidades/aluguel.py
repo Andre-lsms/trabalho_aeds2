110bb32618249b70f411e94d54c2a53f1d4999a8
André Santos <andreluismagalhaes14@gmail.com>
2025-01-09 21:55:22 -0300

    Finalizando a classe aluguel
    corrigi o tratamento dos dados para criar a base

--- Entidades/aluguel.py
+++ Entidades/aluguel.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 from Entidades.entidade_base import EntidadeBase
 from Entidades import funcoes
 from Entidades.filial import Filial
@@ -14,7 +16,7 @@ class Aluguel(EntidadeBase):
     @classmethod
     def get_formato(cls):
         # Define o formato do registro para Aluguel
-        return '=i34s34si34s34s'
+        return '=ii30si30s30sii30si30s'
 
     def criar_registro(self, cod, **kwargs):
         arquivo_cliente = kwargs.get('arquivo_cliente')
@@ -23,25 +25,32 @@ class Aluguel(EntidadeBase):
         arquivo_filial = kwargs.get('arquivo_filial')
         codigo = cod
         cliente = (funcoes.escolher_registro(arquivo_cliente, clientes))[:34]
+        id_cliente = cliente[0]
+        nome_cliente = cliente[1]
         carro = (funcoes.escolher_registro(arquivo_carro, carros))[:34]
+        id_carro = carro[0]
+        modelo_carro = carro[1]
+        marca_carro = carro[3]
         diaria = self.fake.random_int(min=100, max=1000)
         filial_aluguel = (funcoes.escolher_registro(arquivo_filial, filiais))[:34]
+        id_filia_aluguel = filial_aluguel[0]
+        nome_filial_aluguel = filial_aluguel[1]
         filial_devolucao = (funcoes.escolher_registro(arquivo_filial, filiais))[:34]
-
-        return codigo, cliente[0], cliente[1], carro[0], carro[
-            1], diaria, f'[{filial_aluguel[0]}] {filial_aluguel[1]}', f'[{filial_devolucao[0]}] {filial_devolucao[1]}'
+        id_filial_devolucao = filial_devolucao[0]
+        nome_filial_devolucao = filial_devolucao[1]
+        return codigo, id_cliente, nome_cliente, id_carro, modelo_carro,marca_carro, diaria, id_filia_aluguel, nome_filial_aluguel, id_filial_devolucao, nome_filial_devolucao
 
     def imprimir_registro(self, registro):
         registro_lido = self.ler_registro(registro)
         if registro_lido[0] is None:
             return
         print(f"Codigo: [{registro_lido[0]}]")
-        print(f"Cliente: {registro_lido[1]}")
-        print(f"Carro: {registro_lido[2]}")
-        print(f"Diaria: {registro_lido[3]}")
-        print(f"Filial de Aluguel: {registro_lido[4]}")
-        print(f"Filial de Devolucao: {registro_lido[5]}")
-        print(f'{80 * "-"}')
+        print(f"Cliente: [{registro_lido[1]}] {registro_lido[2].decode('utf-8').rstrip(chr(0))}")
+        print(f"Carro: [{registro_lido[3]}] {registro_lido[4].decode('utf-8').rstrip(chr(0))} - {registro_lido[5].decode('utf-8').rstrip(chr(0))}")
+        print(f"Diaria: R$ {registro_lido[6]}")
+        print(f"Filial de Aluguel: [{registro_lido[7]}] {registro_lido[8].decode('utf-8').rstrip(chr(0))}")
+        print(f"Filial de Devolucao: [{registro_lido[9]}] {registro_lido[10].decode('utf-8').rstrip(chr(0))}")
+        print(80 * '-')
 
     def criar_base(self, tamanho, **kwargs):
         arquivo_aluguel = kwargs.get('arquivo_aluguel')
@@ -52,6 +61,7 @@ class Aluguel(EntidadeBase):
         codigos = []
         for i in range(tamanho):
             codigos.append(i + 1)
+        shuffle(codigos)
         for i in codigos:
             registro_criado = (self.criar_registro(i, arquivo_aluguel=arquivo_aluguel, arquivo_cliente=arquivo_cliente,
                                                    arquivo_carro=arquivo_carro, arquivo_filial=arquivo_filial))
